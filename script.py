b5b59235e28a261e0607ee1a468e856cefaa9bd8
Anastasya <barsuk.nastya1@gmail.com>
2019-05-27 23:37:19 +0500

    function decomposition

--- script.py
+++ script.py
@@ -4,6 +4,10 @@ from PIL import ImageDraw
 import os
 import sys
 
+input_path = "source-images"
+height_margin = 90
+width_margin = 30
+
 def get_output_path():
     import shutil
     if sys.argv and len(sys.argv) > 1:
@@ -15,23 +19,22 @@ def get_output_path():
     os.mkdir(output_path)
     return output_path
 
+def main():
+    output_path = get_output_path()
+    images_list = os.listdir(input_path)
+    for image in images_list:
+        if image.endswith('.jpg'):
+            output_image_path = output_path + '/' + image
+            img = Image.open(input_path + '/' + image)
+            draw = ImageDraw.Draw(img)
+            font = ImageFont.truetype("AftaSansThin-Regular.otf", 64)
+            width, height = img.size
+            image_text = "(c) " + image.replace('.jpg', '').replace('-', ' ').title()
+            text_lenght = len(image_text)
+            draw.text((width - width_margin * (text_lenght + 1), height - height_margin),
+                      image_text, (255, 255, 255), font=font)
+            img.save(output_image_path)
 
-output_path = get_output_path()
-input_path = "source-images"
-images_list = os.listdir(input_path)
-height_margin = 90
-width_margin = 30
 
-for image in images_list:
-    if image.endswith('.jpg'):
-        output_image_path = output_path + '/' + image
-        img = Image.open(input_path + '/' + image)
-        draw = ImageDraw.Draw(img)
-        font = ImageFont.truetype("AftaSansThin-Regular.otf", 64)
-        width, height = img.size
-        image_text = "(c) " + image.replace('.jpg', '').replace('-', ' ').title()
-        text_lenght = len(image_text)
-        draw.text((width - width_margin * (text_lenght + 1), height - height_margin),
-                  image_text, (255, 255, 255), font=font)
-        img.save(output_image_path)
-
+if __name__ == '__main__':
+    main()
